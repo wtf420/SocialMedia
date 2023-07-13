061ffb93cc451eee9992d18fc5dc9791b8b3aa0f
vsinh <20520737@gm.uit.edu.vn>
2023-07-13 21:00:23 +0700

    component add

---
+++ Frontend/components/ui/ChatMessage.jsx
@@ -0,0 +1,78 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Image } from "react-native-animatable";
+
+export default function ChatMessage({ chat, userId, avatarSource }) {
+    const status = chat.senderId !== userId;
+    const rawTime = new Date(chat.createdAt);
+    const time = rawTime.toLocaleString();
+
+    return (
+        <View
+            style={
+                status
+                    ? styles.mmessageWrapper
+                    : [styles.mmessageWrapper, { alignItems: "flex-end" }]
+            }
+        >
+            <View style={{ flexDirection: "row" }}>
+                {status && (
+                    <Image
+                        style={{
+                            width: 50,
+                            height: 50,
+                            borderRadius: 25,
+                            marginRight: 10,
+                        }}
+                        source={avatarSource}
+                    />
+                )}
+                <View
+                    style={
+                        status
+                            ? styles.mmessage
+                            : [
+                                  styles.mmessage,
+                                  { backgroundColor: "rgb(194, 243, 194)" },
+                              ]
+                    }
+                >
+                    {chat.message ? <Text>{chat.message}</Text> : null}
+
+                    {chat.imageLink ? (
+                        <Image
+                            source={{ uri: chat.imageLink }}
+                            style={{
+                                width: 150,
+                                height: 150,
+                                marginHorizontal: 3,
+                                marginTop: 7,
+                            }}
+                        />
+                    ) : null}
+                </View>
+            </View>
+
+            <Text style={{ marginLeft: 8 }}>{time}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    mavatar: {
+        width: 10,
+        height: 10,
+    },
+    mmessageWrapper: {
+        width: "100%",
+        alignItems: "flex-start",
+        marginBottom: 15,
+    },
+    mmessage: {
+        maxWidth: "50%",
+        backgroundColor: "#f5ccc2",
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 2,
+    },
+});
--- Frontend/components/ui/FriendList.jsx
+++ Frontend/components/ui/FriendList.jsx
@@ -1,84 +1,88 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useEffect, useState} from 'react';
-import {View, Text, FlatList, Image, Pressable} from 'react-native';
-import ItemRequestUser from './ItemRequestUser';
-//import {createRequestByEmail, getRecommendFr} from '../../api/friendApi';
-import {useSelector} from 'react-redux';
-//import {RootState} from '../../reducers/Store';
-import {Toast} from './Toast';
-//import {useIsFocused} from '@react-navigation/native';
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, Image, Pressable } from "react-native";
+import ItemRequestUser from "./ItemRequestUser";
+import { createRequestByEmail, getRecommendFr } from "../../api/friendApi";
+import { useSelector } from "react-redux";
+import { RootState } from "../../reducers/Store";
+import { Toast } from "./Toast";
+import { useIsFocused } from "@react-navigation/native";
 
-// const FriendList = ({navigation}) => {
-//   const [friends, setFriends] = useState;
-//   const token = useSelector((state) => state.token.key);
-//   const uid = useSelector((stata) => stata.uid.id);
+const FriendList = ({ navigation }) => {
+    const [friends, setFriends] = useState;
+    const token = useSelector((state) => state.token.key);
+    const uid = useSelector((stata) => stata.uid.id);
 
-//   //const isFocused = useIsFocused();
+    const isFocused = useIsFocused();
 
-//   // const handleAddFr = async (email) => {
-//   //   // console.log(email);
-//   //   createRequestByEmail(email, uid, token)
-//   //     .then((response) => {
-//   //       if (response.status === 200) {
-//   //         return response.data;
-//   //       } else {
-//   //         console.log(response.status);
-//   //         throw new Error(response.data.errorMessage);
-//   //       }
-//   //     })
-//   //     .then(data => {
-//   //       console.log(data);
-//   //       setFriends((prev) => {
-//   //         return prev.map((item) => {
-//   //           if (item.email === email) {
-//   //             return {
-//   //               ...item,
-//   //               isFriend: 'pending',
-//   //             };
-//   //           }
-//   //           return item;
-//   //         });
-//   //       });
-//   //     })
-//   //     .catch(error => {
-//   //       Toast(error.message);
-//   //     });
-//   // };
+    const handleAddFr = async (email) => {
+        // console.log(email);
+        createRequestByEmail(email, uid, token)
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.data;
+                } else {
+                    console.log(response.status);
+                    throw new Error(response.data.errorMessage);
+                }
+            })
+            .then((data) => {
+                console.log(data);
+                setFriends((prev) => {
+                    return prev.map((item) => {
+                        if (item.email === email) {
+                            return {
+                                ...item,
+                                isFriend: "pending",
+                            };
+                        }
+                        return item;
+                    });
+                });
+            })
+            .catch((error) => {
+                Toast(error.message);
+            });
+    };
 
-//   // useEffect(() => {
-//   //   const getRecomendFriends = async () => {
-//   //     try {
-//   //       const response = await getRecommendFr(uid, token);
-//   //       if (response.status === 200) {
-//   //         setFriends(response.data);
-//   //       } else {
-//   //         throw new Error(response.data.errorMessage);
-//   //       }
-//   //     } catch (error) {
-//   //       Toast(error.message);
-//   //     }
-//   //   };
-//   //   if (isFocused) getRecomendFriends();
-//   // }, [isFocused]);
+    useEffect(() => {
+        const getRecomendFriends = async () => {
+            try {
+                const response = await getRecommendFr(uid, token);
+                if (response.status === 200) {
+                    setFriends(response.data);
+                } else {
+                    throw new Error(response.data.errorMessage);
+                }
+            } catch (error) {
+                Toast(error.message);
+            }
+        };
+        if (isFocused) getRecomendFriends();
+    }, [isFocused]);
 
-//   return (
-//     <FlatList
-//       data={friends?.slice(0, 2)}
-//       renderItem={({item}) => (
-//         <ItemRequestUser
-//           item={item}
-//           navigation={navigation}
-//           nameRequest={item.isFriend === 'pending' ? 'Pending' : 'Add Friend'}
-//           nameRequest2="Show Profile"
-//           //pressLeft={() => handleAddFr(item.email)}
-//           pressRight={() => navigation.navigate('profileOther', {id: item._id})}
-//         />
-//       )}
-//       keyExtractor={item => item._id}
-//       showsVerticalScrollIndicator={false}
-//       scrollEnabled={false}
-//     />
-//   );
-// };
+    return (
+        <FlatList
+            data={friends?.slice(0, 2)}
+            renderItem={({ item }) => (
+                <ItemRequestUser
+                    item={item}
+                    navigation={navigation}
+                    nameRequest={
+                        item.isFriend === "pending" ? "Pending" : "Add Friend"
+                    }
+                    nameRequest2="Show Profile"
+                    pressLeft={() => handleAddFr(item.email)}
+                    pressRight={() =>
+                        navigation.navigate("profileOther", { id: item._id })
+                    }
+                />
+            )}
+            keyExtractor={(item) => item._id}
+            showsVerticalScrollIndicator={false}
+            scrollEnabled={false}
+        />
+    );
+};
 
 export default FriendList;
