9a37e3ce1e914c82fe241fb1af08ea826bc9cf6b
Duy-0470 <20520470@gm.uit.edu.vn>
2023-07-13 19:14:38 +0700

    Chạy thử 1 tí

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import LoginScreen from "./Screens/LoginScreen";
+import ChatRoom from "./Screens/chatScreens/ChatRoom";
+import ChatScreen from "./Screens/chatScreens/ChatScreen";
+import VideoCallScreen from "./Screens/chatScreens/VideoCallScreen";
 import ChangePassCreen from "./Screens/ChangePassScreen";
+import DetailStatusScreen from "./Screens/DetailStatusScreen";
+import EditPostScreen from "./Screens/EditPostScreen";
+import EditProfileScreen from "./Screens/EditProfileScreen";
+import FirstTimeUserScreen from "./Screens/FirstTimeUseScreen";
+import LoginScreen from "./Screens/LoginScreen";
 import SignUpHrScreen from "./Screens/SignUpScreen";
 import NotificationsScreen from "./Screens/NotificationsScreen";
 
+
 export default function App() {
-    return NotificationsScreen();
+    return FirstTimeUserScreen();
 }
 
 const styles = StyleSheet.create({
