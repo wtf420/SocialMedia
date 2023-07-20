b6fee7b0412e6f3d2f2c1bf9f8b5b331ae8713d1
Duy-0470 <20520470@gm.uit.edu.vn>
2023-07-20 22:32:44 +0700

    Minor edit UI

--- Frontend/components/ui/Header.jsx
+++ Frontend/components/ui/Header.jsx
@@ -70,7 +70,7 @@ const styles = StyleSheet.create({
         flex: 1,
         marginHorizontal: 10,
         height: 40,
-        backgroundColor: Colors.skyBlue,
+        backgroundColor: Colors.gray,
         borderRadius: 5,
         paddingHorizontal: 10,
         color: Colors.black,
